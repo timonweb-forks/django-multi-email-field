5eeab314e631df586e7c87fb9e2380735dc525a5
Tim Kamanin <timur@kamanin.com>
2022-02-18 20:29:35 +0100

    Adds Django 4.0 support

--- multi_email_field/forms.py
+++ multi_email_field/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
+try:
+    from django.utils.translation import ugettext_lazy as _
+except ImportError:
+    from django.utils.translation import gettext_lazy as _
+    
 from multi_email_field.widgets import MultiEmailWidget
 
 
